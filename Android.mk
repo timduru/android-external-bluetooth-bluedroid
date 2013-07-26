c8fbf3ce8f16486be10b3994d0ac3a0951e71c74
timduru <timduru@gmail.com>
2013-07-26 21:49:38 +0200

    fix compile
    Change-Id: I197088d2def1bd4899e34912c34b4932d366e41d

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 # Setup bdroid local make variables for handling configuration
 ifneq ($(BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR),)
   bdroid_C_INCLUDES := $(BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR)
-  bdroid_CFLAGS := -DHAS_BDROID_BUILDCFG
+  bdroid_CFLAGS := -DHAS_BDROID_BUILDCFG -Wno-error=strict-aliasing
 else
   bdroid_C_INCLUDES :=
   bdroid_CFLAGS := -DHAS_NO_BDROID_BUILDCFG
